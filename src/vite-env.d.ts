ee48c21cd7d3b07b5325cbe5970d289530bfaa85
Emilio Cadario <Emilio.Cadario@gmail.com>
2024-12-08 15:31:15 +0100

    fix pr-preview

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string
   readonly VITE_API_BASE_URL: string
   readonly VITE_APP_AUTH_BASE_URL: string
   readonly VITE_APP_REFRESH_AUTH_BASE_URL: string
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  base: '/my-lego-sw-collection',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    base: env.VITE_BASE_URL,
+  }
 })
